b44e467c74d8135ab1babf954e076fe12f576fe6
Guru <jswebguru@hotmail.com>
2024-09-19 17:57:31 +0200

    : Fixed missing 1 year row.

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,4 +1,6 @@
 import json
+import logging
+
 import pandas as pd
 
 
@@ -10,18 +12,26 @@ def json_to_csv(input_path, output_path):
     # Load each section into its own DataFrame
     company_info = pd.DataFrame(json_input["company"])
     nominee_df = pd.DataFrame(json_input["nominees"])
-    proposal_df = pd.DataFrame(json_input["proposals"])
-    proposal_time_df = pd.DataFrame(json_input["proposal_time"])
+    # nominee_df.insert(0, 'No', range(1.1, 1.1 + len(nominee_df) * 0.1, 0.1))
+    # nominee_df.insert(0, 'No', range(1.1, 1.1 + len(nominee_df) * 0.1, 0.1))
 
+    proposal_df = pd.DataFrame(json_input["proposals"])
+    try:
+        proposal_time_df = pd.DataFrame(json_input["proposal_time"])
+    except Exception as e:
+        print('There is no odd proposal in this document.', e)
     # Write each section out to the CSV file, separated by blank lines
-    with open(output_path, "w", newline='', encoding='utf-8') as file:
+    with open(output_path, "w", newline='') as file:
         company_info.to_csv(file, index=False)
         file.write("\n")  # Blank line to separate sections
         nominee_df.to_csv(file, index=False)
         file.write("\n")  # Blank line to separate sections
         proposal_df.to_csv(file, index=False)
         file.write("\n")  # Blank line to separate sections
-        proposal_time_df.to_csv(file, index=False)
+        try:
+            proposal_time_df.to_csv(file, index=False)
+        except Exception as e:
+            pass
 
 if __name__ == '__main__':
-    json_to_csv('output/brhc10049413_8k.json', 'output/brhc10049413_8k.csv')
+    json_to_csv('output/tm2315877d1_8k.json', 'output/tm2315877d1_8k.csv')

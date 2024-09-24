46969b2bd2624daef8821d913a65c9b5f7c46a9a
Guru <jswebguru@hotmail.com>
2024-09-25 01:48:37 +0900

    : Fixed numbering algorithm again.

--- json_to_csv.py
+++ json_to_csv.py
@@ -1,6 +1,4 @@
 import json
-import logging
-
 import pandas as pd
 
 
@@ -11,14 +9,13 @@ def json_to_csv(input_path, output_path):
     # Load each section into its own DataFrame
     company_info = pd.DataFrame(json_input["company"])
     nominee_df = pd.DataFrame(json_input["nominees"])
+    nominee_df.insert(0, 'No', ['1.' + str(x + 1) for x in range(len(nominee_df))])
     # nominee_df.insert(0, 'No', range(1.1, 1.1 + len(nominee_df) * 0.1, 0.1))
-    # nominee_df.insert(0, 'No', range(1.1, 1.1 + len(nominee_df) * 0.1, 0.1))
+    nominee_df['No'] = nominee_df['No'].apply(lambda x: f"{x}.")
 
     proposal_df = pd.DataFrame(json_input["proposals"])
-    try:
-        proposal_time_df = pd.DataFrame(json_input["proposal_time"])
-    except Exception as e:
-        print('There is no odd proposal in this document.', e)
+
+    proposal_df.insert(0, 'No', list(range(2, proposal_df.__len__() + 2)))
     # Write each section out to the CSV file, separated by blank lines
     with open(output_path, "w", newline='') as file:
         company_info.to_csv(file, index=False)
@@ -27,10 +24,7 @@ def json_to_csv(input_path, output_path):
         file.write("\n")  # Blank line to separate sections
         proposal_df.to_csv(file, index=False)
         file.write("\n")  # Blank line to separate sections
-        try:
-            proposal_time_df.to_csv(file, index=False)
-        except Exception as e:
-            pass
+
 
 if __name__ == '__main__':
-    json_to_csv('output/tm2315877d1_8k.json', 'output/tm2315877d1_8k.csv')
+    json_to_csv('output/tm2315877d1_8k.json', 'tm2315877d1_8k.csv')
